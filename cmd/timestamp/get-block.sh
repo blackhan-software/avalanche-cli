gin/ava-cli

CLI: timestamp/*.sh

cmd/timestamp/get-block.sh
@@ -1,0 +1,94 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC1090,SC2214
+###############################################################################
+PLATFORM_SCRIPT=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)
+###############################################################################
+
+source "$PLATFORM_SCRIPT/../../cli/color.sh" ;
+source "$PLATFORM_SCRIPT/../../cli/command.sh" ;
+source "$PLATFORM_SCRIPT/../../cli/rpc/data.sh" ;
+source "$PLATFORM_SCRIPT/../../cli/rpc/post.sh" ;
+
+###############################################################################
+###############################################################################
+
+function cli_help {
+    local usage ;
+    usage="${BB}Usage:${NB} $(command_fqn "${0}")" ;
+    usage+=" [-i|--id=\${AVA_ID=''}]" ;
+    usage+=" [-N|--node=\${AVA_NODE-127.0.0.1:9650}]" ;
+    usage+=" [-S|--silent-rpc|\${AVA_SILENT_RPC}]" ;
+    usage+=" [-V|--verbose-rpc|\${AVA_VERBOSE_RPC}]" ;
+    usage+=" [-Y|--yes-run-rpc|\${AVA_YES_RUN_RPC}]" ;
+    usage+=" [-h|--help]" ;
+    source "$PLATFORM_SCRIPT/../../cli/help.sh" ; # shellcheck disable=2046
+    printf '%s\n\n%s\n' "$usage" "$(help_for $(command_fqn "${0}"))" ;
+}
+
+function cli_options {
+    local -a options ;
+    options+=( "-i" "--id=" ) ;
+    options+=( "-N" "--node=" ) ;
+    options+=( "-S" "--silent-rpc" ) ;
+    options+=( "-V" "--verbose-rpc" ) ;
+    options+=( "-Y" "--yes-run-rpc" ) ;
+    options+=( "-h" "--help" ) ;
+    printf '%s ' "${options[@]}" ;
+}
+
+function cli {
+    while getopts ":hSVYN:i:-:" OPT "$@"
+    do
+        if [ "$OPT" = "-" ] ; then
+            OPT="${OPTARG%%=*}" ;
+            OPTARG="${OPTARG#$OPT}" ;
+            OPTARG="${OPTARG#=}" ;
+        fi
+        case "${OPT}" in
+            list-options)
+                cli_options && exit 0 ;;
+            i|id)
+                AVA_ID="${OPTARG}" ;;
+            N|node)
+                AVA_NODE="${OPTARG}" ;;
+            S|silent-rpc)
+                export AVA_SILENT_RPC=1 ;;
+            V|verbose-rpc)
+                export AVA_VERBOSE_RPC=1 ;;
+            Y|yes-run-rpc)
+                export AVA_YES_RUN_RPC=1 ;;
+            h|help)
+                cli_help && exit 0 ;;
+            :|*)
+                cli_help && exit 1 ;;
+        esac
+    done
+    if [ -z "$AVA_ID" ] ; then
+        AVA_ID="" ;
+    fi
+    if [ -z "$AVA_NODE" ] ; then
+        AVA_NODE="127.0.0.1:9650" ;
+    fi
+    shift $((OPTIND-1)) ;
+}
+
+function rpc_method {
+    printf "timestamp.getBlock" ;
+}
+
+function rpc_params {
+    printf '{' ;
+    if [ -n "$AVA_ID" ] ; then
+        printf '"id":"%s"' "$AVA_ID" ;
+    else
+        printf '"id":null' ;
+    fi
+    printf '}' ;
+}
+
+###############################################################################
+
+cli "$@" && rpc_post "$AVA_NODE/ext/bc/timestamp" "$(rpc_data)" ;
+
+###############################################################################
+###############################################################################

cmd/timestamp/get-block.test.sh
@@ -1,0 +1,43 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2091
+###############################################################################
+
+function cmd {
+    printf "./ava-cli.sh timestamp get-block" ;
+}
+
+function check {
+    local result="$1" ;
+    local result_u ; result_u=$(printf '%s' "$result" | cut -d' ' -f3) ;
+    local result_h ; result_h=$(printf '%s' "$result" | cut -d' ' -f5) ;
+    local result_d ; result_d=$(printf '%s' "$result" | cut -d' ' -f7) ;
+    local expect_u ; expect_u="'127.0.0.1:9650/ext/bc/timestamp'" ;
+    assertEquals "$expect_u" "$result_u" ;
+    local expect_h ; expect_h="'content-type:application/json'" ;
+    assertEquals "$expect_h" "$result_h" ;
+    local expect_d ; expect_d="'{" ;
+    expect_d+='"jsonrpc":"2.0",' ;
+    expect_d+='"id":1,' ;
+    expect_d+='"method":"timestamp.getBlock",' ;
+    expect_d+='"params":{' ;
+    expect_d+='"id":'"$([ -n "$2" ] && echo "\"$2\"" || echo null)" ;
+    expect_d+="}}'" ;
+    assertEquals "$expect_d" "$result_d" ;
+    local expect="curl --url $expect_u --header $expect_h --data $expect_d" ;
+    assertEquals "$expect" "$result" ;
+}
+
+function test_timestamp__get_block_1a {
+    check "$(RPC_ID=1 $(cmd) -i ID)" ID ;
+}
+
+function test_timestamp__get_block_1b {
+    check "$(RPC_ID=1 AVA_ID=ID $(cmd))" ID ;
+}
+
+function test_timestamp__get_block_1c {
+    check "$(RPC_ID=1 $(cmd))" ;
+}
+
+###############################################################################
+###############################################################################

cmd/timestamp/propose-block.sh
@@ -1,0 +1,90 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC1090,SC2214
+###############################################################################
+PLATFORM_SCRIPT=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)
+###############################################################################
+
+source "$PLATFORM_SCRIPT/../../cli/color.sh" ;
+source "$PLATFORM_SCRIPT/../../cli/command.sh" ;
+source "$PLATFORM_SCRIPT/../../cli/rpc/data.sh" ;
+source "$PLATFORM_SCRIPT/../../cli/rpc/post.sh" ;
+
+###############################################################################
+###############################################################################
+
+function cli_help {
+    local usage ;
+    usage="${BB}Usage:${NB} $(command_fqn "${0}")" ;
+    usage+=" [-d|--data=\${AVA_DATA}]" ;
+    usage+=" [-N|--node=\${AVA_NODE-127.0.0.1:9650}]" ;
+    usage+=" [-S|--silent-rpc|\${AVA_SILENT_RPC}]" ;
+    usage+=" [-V|--verbose-rpc|\${AVA_VERBOSE_RPC}]" ;
+    usage+=" [-Y|--yes-run-rpc|\${AVA_YES_RUN_RPC}]" ;
+    usage+=" [-h|--help]" ;
+    source "$PLATFORM_SCRIPT/../../cli/help.sh" ; # shellcheck disable=2046
+    printf '%s\n\n%s\n' "$usage" "$(help_for $(command_fqn "${0}"))" ;
+}
+
+function cli_options {
+    local -a options ;
+    options+=( "-d" "--data=" ) ;
+    options+=( "-N" "--node=" ) ;
+    options+=( "-S" "--silent-rpc" ) ;
+    options+=( "-V" "--verbose-rpc" ) ;
+    options+=( "-Y" "--yes-run-rpc" ) ;
+    options+=( "-h" "--help" ) ;
+    printf '%s ' "${options[@]}" ;
+}
+
+function cli {
+    while getopts ":hSVYN:d:-:" OPT "$@"
+    do
+        if [ "$OPT" = "-" ] ; then
+            OPT="${OPTARG%%=*}" ;
+            OPTARG="${OPTARG#$OPT}" ;
+            OPTARG="${OPTARG#=}" ;
+        fi
+        case "${OPT}" in
+            list-options)
+                cli_options && exit 0 ;;
+            d|data)
+                AVA_DATA="${OPTARG}" ;;
+            N|node)
+                AVA_NODE="${OPTARG}" ;;
+            S|silent-rpc)
+                export AVA_SILENT_RPC=1 ;;
+            V|verbose-rpc)
+                export AVA_VERBOSE_RPC=1 ;;
+            Y|yes-run-rpc)
+                export AVA_YES_RUN_RPC=1 ;;
+            h|help)
+                cli_help && exit 0 ;;
+            :|*)
+                cli_help && exit 1 ;;
+        esac
+    done
+    if [ -z "$AVA_DATA" ] ; then
+        cli_help && exit 1 ;
+    fi
+    if [ -z "$AVA_NODE" ] ; then
+        AVA_NODE="127.0.0.1:9650" ;
+    fi
+    shift $((OPTIND-1)) ;
+}
+
+function rpc_method {
+    printf "timestamp.proposeBlock" ;
+}
+
+function rpc_params {
+    printf '{' ;
+    printf '"data":"%s"' "$AVA_DATA" ;
+    printf '}' ;
+}
+
+###############################################################################
+
+cli "$@" && rpc_post "$AVA_NODE/ext/bc/timestamp" "$(rpc_data)" ;
+
+###############################################################################
+###############################################################################

cmd/timestamp/propose-block.test.sh
@@ -1,0 +1,39 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2091
+###############################################################################
+
+function cmd {
+    printf "./ava-cli.sh timestamp propose-block" ;
+}
+
+function check {
+    local result="$1" ;
+    local result_u ; result_u=$(printf '%s' "$result" | cut -d' ' -f3) ;
+    local result_h ; result_h=$(printf '%s' "$result" | cut -d' ' -f5) ;
+    local result_d ; result_d=$(printf '%s' "$result" | cut -d' ' -f7) ;
+    local expect_u ; expect_u="'127.0.0.1:9650/ext/bc/timestamp'" ;
+    assertEquals "$expect_u" "$result_u" ;
+    local expect_h ; expect_h="'content-type:application/json'" ;
+    assertEquals "$expect_h" "$result_h" ;
+    local expect_d ; expect_d="'{" ;
+    expect_d+='"jsonrpc":"2.0",' ;
+    expect_d+='"id":1,' ;
+    expect_d+='"method":"timestamp.proposeBlock",' ;
+    expect_d+='"params":{' ;
+    expect_d+='"data":"DATA"' ;
+    expect_d+="}}'" ;
+    assertEquals "$expect_d" "$result_d" ;
+    local expect="curl --url $expect_u --header $expect_h --data $expect_d" ;
+    assertEquals "$expect" "$result" ;
+}
+
+function test_timestamp__propose_block_1a {
+    check "$(RPC_ID=1 $(cmd) -d DATA)" ID ;
+}
+
+function test_timestamp__propose_block_1a {
+    check "$(RPC_ID=1 AVA_DATA=DATA $(cmd))" ID ;
+}
+
+###############################################################################
+###############################################################################

blackhan-software/avalanche-cli

cmd/timestamp/get-block.sh
@@ -1,0 +1,94 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC1090,SC2214
+###############################################################################
+CMD_SCRIPT=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)
+###############################################################################
+
+source "$CMD_SCRIPT/../../cli/color.sh" ;
+source "$CMD_SCRIPT/../../cli/command.sh" ;
+source "$CMD_SCRIPT/../../cli/rpc/data.sh" ;
+source "$CMD_SCRIPT/../../cli/rpc/post.sh" ;
+
+###############################################################################
+###############################################################################
+
+function cli_help {
+    local usage ;
+    usage="${BB}Usage:${NB} $(command_fqn "${0}")" ;
+    usage+=" [-i|--id=\${AVA_ID=''}]" ;
+    usage+=" [-N|--node=\${AVA_NODE-127.0.0.1:9650}]" ;
+    usage+=" [-S|--silent-rpc|\${AVA_SILENT_RPC}]" ;
+    usage+=" [-V|--verbose-rpc|\${AVA_VERBOSE_RPC}]" ;
+    usage+=" [-Y|--yes-run-rpc|\${AVA_YES_RUN_RPC}]" ;
+    usage+=" [-h|--help]" ;
+    source "$CMD_SCRIPT/../../cli/help.sh" ; # shellcheck disable=2046
+    printf '%s\n\n%s\n' "$usage" "$(help_for $(command_fqn "${0}"))" ;
+}
+
+function cli_options {
+    local -a options ;
+    options+=( "-i" "--id=" ) ;
+    options+=( "-N" "--node=" ) ;
+    options+=( "-S" "--silent-rpc" ) ;
+    options+=( "-V" "--verbose-rpc" ) ;
+    options+=( "-Y" "--yes-run-rpc" ) ;
+    options+=( "-h" "--help" ) ;
+    printf '%s ' "${options[@]}" ;
+}
+
+function cli {
+    while getopts ":hSVYN:i:-:" OPT "$@"
+    do
+        if [ "$OPT" = "-" ] ; then
+            OPT="${OPTARG%%=*}" ;
+            OPTARG="${OPTARG#$OPT}" ;
+            OPTARG="${OPTARG#=}" ;
+        fi
+        case "${OPT}" in
+            list-options)
+                cli_options && exit 0 ;;
+            i|id)
+                AVA_ID="${OPTARG}" ;;
+            N|node)
+                AVA_NODE="${OPTARG}" ;;
+            S|silent-rpc)
+                export AVA_SILENT_RPC=1 ;;
+            V|verbose-rpc)
+                export AVA_VERBOSE_RPC=1 ;;
+            Y|yes-run-rpc)
+                export AVA_YES_RUN_RPC=1 ;;
+            h|help)
+                cli_help && exit 0 ;;
+            :|*)
+                cli_help && exit 1 ;;
+        esac
+    done
+    if [ -z "$AVA_ID" ] ; then
+        AVA_ID="" ;
+    fi
+    if [ -z "$AVA_NODE" ] ; then
+        AVA_NODE="127.0.0.1:9650" ;
+    fi
+    shift $((OPTIND-1)) ;
+}
+
+function rpc_method {
+    printf "timestamp.getBlock" ;
+}
+
+function rpc_params {
+    printf '{' ;
+    if [ -n "$AVA_ID" ] ; then
+        printf '"id":"%s"' "$AVA_ID" ;
+    else
+        printf '"id":null' ;
+    fi
+    printf '}' ;
+}
+
+###############################################################################
+
+cli "$@" && rpc_post "$AVA_NODE/ext/bc/timestamp" "$(rpc_data)" ;
+
+###############################################################################
+###############################################################################

cmd/timestamp/get-block.test.sh
@@ -1,0 +1,43 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2091
+###############################################################################
+
+function cmd {
+    printf "./avalanche-cli.sh timestamp get-block" ;
+}
+
+function check {
+    local result="$1" ;
+    local result_u ; result_u=$(printf '%s' "$result" | cut -d' ' -f3) ;
+    local result_h ; result_h=$(printf '%s' "$result" | cut -d' ' -f5) ;
+    local result_d ; result_d=$(printf '%s' "$result" | cut -d' ' -f7) ;
+    local expect_u ; expect_u="'127.0.0.1:9650/ext/bc/timestamp'" ;
+    assertEquals "$expect_u" "$result_u" ;
+    local expect_h ; expect_h="'content-type:application/json'" ;
+    assertEquals "$expect_h" "$result_h" ;
+    local expect_d ; expect_d="'{" ;
+    expect_d+='"jsonrpc":"2.0",' ;
+    expect_d+='"id":1,' ;
+    expect_d+='"method":"timestamp.getBlock",' ;
+    expect_d+='"params":{' ;
+    expect_d+='"id":'"$([ -n "$2" ] && echo "\"$2\"" || echo null)" ;
+    expect_d+="}}'" ;
+    assertEquals "$expect_d" "$result_d" ;
+    local expect="curl --url $expect_u --header $expect_h --data $expect_d" ;
+    assertEquals "$expect" "$result" ;
+}
+
+function test_timestamp__get_block_1a {
+    check "$(RPC_ID=1 $(cmd) -i ID)" ID ;
+}
+
+function test_timestamp__get_block_1b {
+    check "$(RPC_ID=1 AVA_ID=ID $(cmd))" ID ;
+}
+
+function test_timestamp__get_block_1c {
+    check "$(RPC_ID=1 $(cmd))" ;
+}
+
+###############################################################################
+###############################################################################

cmd/timestamp/propose-block.sh
@@ -1,0 +1,90 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC1090,SC2214
+###############################################################################
+CMD_SCRIPT=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)
+###############################################################################
+
+source "$CMD_SCRIPT/../../cli/color.sh" ;
+source "$CMD_SCRIPT/../../cli/command.sh" ;
+source "$CMD_SCRIPT/../../cli/rpc/data.sh" ;
+source "$CMD_SCRIPT/../../cli/rpc/post.sh" ;
+
+###############################################################################
+###############################################################################
+
+function cli_help {
+    local usage ;
+    usage="${BB}Usage:${NB} $(command_fqn "${0}")" ;
+    usage+=" [-d|--data=\${AVA_DATA}]" ;
+    usage+=" [-N|--node=\${AVA_NODE-127.0.0.1:9650}]" ;
+    usage+=" [-S|--silent-rpc|\${AVA_SILENT_RPC}]" ;
+    usage+=" [-V|--verbose-rpc|\${AVA_VERBOSE_RPC}]" ;
+    usage+=" [-Y|--yes-run-rpc|\${AVA_YES_RUN_RPC}]" ;
+    usage+=" [-h|--help]" ;
+    source "$CMD_SCRIPT/../../cli/help.sh" ; # shellcheck disable=2046
+    printf '%s\n\n%s\n' "$usage" "$(help_for $(command_fqn "${0}"))" ;
+}
+
+function cli_options {
+    local -a options ;
+    options+=( "-d" "--data=" ) ;
+    options+=( "-N" "--node=" ) ;
+    options+=( "-S" "--silent-rpc" ) ;
+    options+=( "-V" "--verbose-rpc" ) ;
+    options+=( "-Y" "--yes-run-rpc" ) ;
+    options+=( "-h" "--help" ) ;
+    printf '%s ' "${options[@]}" ;
+}
+
+function cli {
+    while getopts ":hSVYN:d:-:" OPT "$@"
+    do
+        if [ "$OPT" = "-" ] ; then
+            OPT="${OPTARG%%=*}" ;
+            OPTARG="${OPTARG#$OPT}" ;
+            OPTARG="${OPTARG#=}" ;
+        fi
+        case "${OPT}" in
+            list-options)
+                cli_options && exit 0 ;;
+            d|data)
+                AVA_DATA="${OPTARG}" ;;
+            N|node)
+                AVA_NODE="${OPTARG}" ;;
+            S|silent-rpc)
+                export AVA_SILENT_RPC=1 ;;
+            V|verbose-rpc)
+                export AVA_VERBOSE_RPC=1 ;;
+            Y|yes-run-rpc)
+                export AVA_YES_RUN_RPC=1 ;;
+            h|help)
+                cli_help && exit 0 ;;
+            :|*)
+                cli_help && exit 1 ;;
+        esac
+    done
+    if [ -z "$AVA_DATA" ] ; then
+        cli_help && exit 1 ;
+    fi
+    if [ -z "$AVA_NODE" ] ; then
+        AVA_NODE="127.0.0.1:9650" ;
+    fi
+    shift $((OPTIND-1)) ;
+}
+
+function rpc_method {
+    printf "timestamp.proposeBlock" ;
+}
+
+function rpc_params {
+    printf '{' ;
+    printf '"data":"%s"' "$AVA_DATA" ;
+    printf '}' ;
+}
+
+###############################################################################
+
+cli "$@" && rpc_post "$AVA_NODE/ext/bc/timestamp" "$(rpc_data)" ;
+
+###############################################################################
+###############################################################################

cmd/timestamp/propose-block.test.sh
@@ -1,0 +1,39 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2091
+###############################################################################
+
+function cmd {
+    printf "./avalanche-cli.sh timestamp propose-block" ;
+}
+
+function check {
+    local result="$1" ;
+    local result_u ; result_u=$(printf '%s' "$result" | cut -d' ' -f3) ;
+    local result_h ; result_h=$(printf '%s' "$result" | cut -d' ' -f5) ;
+    local result_d ; result_d=$(printf '%s' "$result" | cut -d' ' -f7) ;
+    local expect_u ; expect_u="'127.0.0.1:9650/ext/bc/timestamp'" ;
+    assertEquals "$expect_u" "$result_u" ;
+    local expect_h ; expect_h="'content-type:application/json'" ;
+    assertEquals "$expect_h" "$result_h" ;
+    local expect_d ; expect_d="'{" ;
+    expect_d+='"jsonrpc":"2.0",' ;
+    expect_d+='"id":1,' ;
+    expect_d+='"method":"timestamp.proposeBlock",' ;
+    expect_d+='"params":{' ;
+    expect_d+='"data":"DATA"' ;
+    expect_d+="}}'" ;
+    assertEquals "$expect_d" "$result_d" ;
+    local expect="curl --url $expect_u --header $expect_h --data $expect_d" ;
+    assertEquals "$expect" "$result" ;
+}
+
+function test_timestamp__propose_block_1a {
+    check "$(RPC_ID=1 $(cmd) -d DATA)" ID ;
+}
+
+function test_timestamp__propose_block_1a {
+    check "$(RPC_ID=1 AVA_DATA=DATA $(cmd))" ID ;
+}
+
+###############################################################################
+###############################################################################