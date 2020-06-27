gin/ava-cli

CLI: keystore/*.sh

cmd/keystore/create-user.sh
@@ -1,0 +1,98 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC1090,SC2214
+###############################################################################
+KEYSTORE_SCRIPT=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)
+###############################################################################
+
+source "$KEYSTORE_SCRIPT/../../cli/color.sh" ;
+source "$KEYSTORE_SCRIPT/../../cli/command.sh" ;
+source "$KEYSTORE_SCRIPT/../../cli/rpc/data.sh" ;
+source "$KEYSTORE_SCRIPT/../../cli/rpc/post.sh" ;
+
+###############################################################################
+###############################################################################
+
+function cli_help {
+    local usage ;
+    usage="${BB}Usage:${NB} $(command_fqn "${0}")" ;
+    usage+=" [-u|--username=\${AVA_USERNAME}]" ;
+    usage+=" [-p|--password=\${AVA_PASSWORD}]" ;
+    usage+=" [-N|--node=\${AVA_NODE-127.0.0.1:9650}]" ;
+    usage+=" [-S|--silent-rpc|\${AVA_SILENT_RPC}]" ;
+    usage+=" [-V|--verbose-rpc|\${AVA_VERBOSE_RPC}]" ;
+    usage+=" [-Y|--yes-run-rpc|\${AVA_YES_RUN_RPC}]" ;
+    usage+=" [-h|--help]" ;
+    source "$KEYSTORE_SCRIPT/../../cli/help.sh" ; # shellcheck disable=2046
+    printf '%s\n\n%s\n' "$usage" "$(help_for $(command_fqn "${0}"))" ;
+}
+
+function cli_options {
+    local -a options ;
+    options+=( "-u" "--username=" ) ;
+    options+=( "-p" "--password=" ) ;
+    options+=( "-N" "--node=" ) ;
+    options+=( "-S" "--silent-rpc" ) ;
+    options+=( "-V" "--verbose-rpc" ) ;
+    options+=( "-Y" "--yes-run-rpc" ) ;
+    options+=( "-h" "--help" ) ;
+    printf '%s ' "${options[@]}" ;
+}
+
+function cli {
+    while getopts ":hSVYN:u:p:-:" OPT "$@"
+    do
+        if [ "$OPT" = "-" ] ; then
+            OPT="${OPTARG%%=*}" ;
+            OPTARG="${OPTARG#$OPT}" ;
+            OPTARG="${OPTARG#=}" ;
+        fi
+        case "${OPT}" in
+            list-options)
+                cli_options && exit 0 ;;
+            u|username)
+                AVA_USERNAME="${OPTARG}" ;;
+            p|password)
+                AVA_PASSWORD="${OPTARG}" ;;
+            N|node)
+                AVA_NODE="${OPTARG}" ;;
+            S|silent-rpc)
+                export AVA_SILENT_RPC=1 ;;
+            V|verbose-rpc)
+                export AVA_VERBOSE_RPC=1 ;;
+            Y|yes-run-rpc)
+                export AVA_YES_RUN_RPC=1 ;;
+            h|help)
+                cli_help && exit 0 ;;
+            :|*)
+                cli_help && exit 1 ;;
+        esac
+    done
+    if [ -z "$AVA_USERNAME" ] ; then
+        cli_help && exit 1 ;
+    fi
+    if [ -z "$AVA_PASSWORD" ] ; then
+        cli_help && exit 1 ;
+    fi
+    if [ -z "$AVA_NODE" ] ; then
+        AVA_NODE="127.0.0.1:9650" ;
+    fi
+    shift $((OPTIND-1)) ;
+}
+
+function rpc_method {
+    printf "keystore.createUser" ;
+}
+
+function rpc_params {
+    printf '{' ;
+    printf '"username":"%s",' "$AVA_USERNAME" ;
+    printf '"password":"%s"' "$AVA_PASSWORD" ;
+    printf '}' ;
+}
+
+###############################################################################
+
+cli "$@" && rpc_post "$AVA_NODE/ext/keystore" "$(rpc_data)" ;
+
+###############################################################################
+###############################################################################

cmd/keystore/create-user.test.sh
@@ -1,0 +1,43 @@
+#!/usr/bin/env bash
+###############################################################################
+
+function cmd {
+    printf "./ava-cli.sh keystore create-user" ;
+}
+
+function check {
+    local result="$1" ;
+    local result_u ; result_u=$(printf '%s' "$result" | cut -d' ' -f3) ;
+    local result_h ; result_h=$(printf '%s' "$result" | cut -d' ' -f5) ;
+    local result_d ; result_d=$(printf '%s' "$result" | cut -d' ' -f7) ;
+    local expect_u ; expect_u="'127.0.0.1:9650/ext/keystore'" ;
+    assertEquals "$expect_u" "$result_u" ;
+    local expect_h ; expect_h="'content-type:application/json'" ;
+    assertEquals "$expect_h" "$result_h" ;
+    local expect_d ; expect_d="'{" ;
+    expect_d+='"jsonrpc":"2.0",' ;
+    expect_d+='"id":1,' ;
+    expect_d+='"method":"keystore.createUser",' ;
+    expect_d+='"params":{' ;
+    expect_d+='"username":"USERNAME",' ;
+    expect_d+='"password":"PASSWORD"' ;
+    expect_d+="}}'" ;
+    assertEquals "$expect_d" "$result_d" ;
+    local expect="curl --url $expect_u --header $expect_h --data $expect_d" ;
+    assertEquals "$expect" "$result" ;
+}
+
+function test_keystore__create_user_1a {
+    check "$(RPC_ID=1 $(cmd) -u USERNAME -p PASSWORD)" ;
+}
+
+function test_keystore__create_user_1b {
+    check "$(RPC_ID=1 AVA_USERNAME=USERNAME $(cmd) -p PASSWORD)" ;
+}
+
+function test_keystore__create_user_1c {
+    check "$(RPC_ID=1 AVA_PASSWORD=PASSWORD $(cmd) -u USERNAME)" ;
+}
+
+###############################################################################
+###############################################################################

cmd/keystore/delete-user.test.sh
@@ -1,0 +1,43 @@
+#!/usr/bin/env bash
+###############################################################################
+
+function cmd {
+    printf "./ava-cli.sh keystore delete-user" ;
+}
+
+function check {
+    local result="$1" ;
+    local result_u ; result_u=$(printf '%s' "$result" | cut -d' ' -f3) ;
+    local result_h ; result_h=$(printf '%s' "$result" | cut -d' ' -f5) ;
+    local result_d ; result_d=$(printf '%s' "$result" | cut -d' ' -f7) ;
+    local expect_u ; expect_u="'127.0.0.1:9650/ext/keystore'" ;
+    assertEquals "$expect_u" "$result_u" ;
+    local expect_h ; expect_h="'content-type:application/json'" ;
+    assertEquals "$expect_h" "$result_h" ;
+    local expect_d ; expect_d="'{" ;
+    expect_d+='"jsonrpc":"2.0",' ;
+    expect_d+='"id":1,' ;
+    expect_d+='"method":"keystore.deleteUser",' ;
+    expect_d+='"params":{' ;
+    expect_d+='"username":"USERNAME",' ;
+    expect_d+='"password":"PASSWORD"' ;
+    expect_d+="}}'" ;
+    assertEquals "$expect_d" "$result_d" ;
+    local expect="curl --url $expect_u --header $expect_h --data $expect_d" ;
+    assertEquals "$expect" "$result" ;
+}
+
+function test_keystore__delete_user_1a {
+    check "$(RPC_ID=1 $(cmd) -u USERNAME -p PASSWORD)" ;
+}
+
+function test_keystore__delete_user_1b {
+    check "$(RPC_ID=1 AVA_USERNAME=USERNAME $(cmd) -p PASSWORD)" ;
+}
+
+function test_keystore__delete_user_1c {
+    check "$(RPC_ID=1 AVA_PASSWORD=PASSWORD $(cmd) -u USERNAME)" ;
+}
+
+###############################################################################
+###############################################################################

cmd/keystore/export-user.sh
@@ -1,0 +1,98 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC1090,SC2214
+###############################################################################
+KEYSTORE_SCRIPT=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)
+###############################################################################
+
+source "$KEYSTORE_SCRIPT/../../cli/color.sh" ;
+source "$KEYSTORE_SCRIPT/../../cli/command.sh" ;
+source "$KEYSTORE_SCRIPT/../../cli/rpc/data.sh" ;
+source "$KEYSTORE_SCRIPT/../../cli/rpc/post.sh" ;
+
+###############################################################################
+###############################################################################
+
+function cli_help {
+    local usage ;
+    usage="${BB}Usage:${NB} $(command_fqn "${0}")" ;
+    usage+=" [-u|--username=\${AVA_USERNAME}]" ;
+    usage+=" [-p|--password=\${AVA_PASSWORD}]" ;
+    usage+=" [-N|--node=\${AVA_NODE-127.0.0.1:9650}]" ;
+    usage+=" [-S|--silent-rpc|\${AVA_SILENT_RPC}]" ;
+    usage+=" [-V|--verbose-rpc|\${AVA_VERBOSE_RPC}]" ;
+    usage+=" [-Y|--yes-run-rpc|\${AVA_YES_RUN_RPC}]" ;
+    usage+=" [-h|--help]" ;
+    source "$KEYSTORE_SCRIPT/../../cli/help.sh" ; # shellcheck disable=2046
+    printf '%s\n\n%s\n' "$usage" "$(help_for $(command_fqn "${0}"))" ;
+}
+
+function cli_options {
+    local -a options ;
+    options+=( "-u" "--username=" ) ;
+    options+=( "-p" "--password=" ) ;
+    options+=( "-N" "--node=" ) ;
+    options+=( "-S" "--silent-rpc" ) ;
+    options+=( "-V" "--verbose-rpc" ) ;
+    options+=( "-Y" "--yes-run-rpc" ) ;
+    options+=( "-h" "--help" ) ;
+    printf '%s ' "${options[@]}" ;
+}
+
+function cli {
+    while getopts ":hSVYN:u:p:-:" OPT "$@"
+    do
+        if [ "$OPT" = "-" ] ; then
+            OPT="${OPTARG%%=*}" ;
+            OPTARG="${OPTARG#$OPT}" ;
+            OPTARG="${OPTARG#=}" ;
+        fi
+        case "${OPT}" in
+            list-options)
+                cli_options && exit 0 ;;
+            u|username)
+                AVA_USERNAME="${OPTARG}" ;;
+            p|password)
+                AVA_PASSWORD="${OPTARG}" ;;
+            N|node)
+                AVA_NODE="${OPTARG}" ;;
+            S|silent-rpc)
+                export AVA_SILENT_RPC=1 ;;
+            V|verbose-rpc)
+                export AVA_VERBOSE_RPC=1 ;;
+            Y|yes-run-rpc)
+                export AVA_YES_RUN_RPC=1 ;;
+            h|help)
+                cli_help && exit 0 ;;
+            :|*)
+                cli_help && exit 1 ;;
+        esac
+    done
+    if [ -z "$AVA_USERNAME" ] ; then
+        cli_help && exit 1 ;
+    fi
+    if [ -z "$AVA_PASSWORD" ] ; then
+        cli_help && exit 1 ;
+    fi
+    if [ -z "$AVA_NODE" ] ; then
+        AVA_NODE="127.0.0.1:9650" ;
+    fi
+    shift $((OPTIND-1)) ;
+}
+
+function rpc_method {
+    printf "keystore.exportUser" ;
+}
+
+function rpc_params {
+    printf '{' ;
+    printf '"username":"%s",' "$AVA_USERNAME" ;
+    printf '"password":"%s"' "$AVA_PASSWORD" ;
+    printf '}' ;
+}
+
+###############################################################################
+
+cli "$@" && rpc_post "$AVA_NODE/ext/keystore" "$(rpc_data)" ;
+
+###############################################################################
+###############################################################################

cmd/keystore/import-user.test.sh
@@ -1,0 +1,48 @@
+#!/usr/bin/env bash
+###############################################################################
+
+function cmd {
+    printf "./ava-cli.sh keystore import-user" ;
+}
+
+function check {
+    local result="$1" ;
+    local result_u ; result_u=$(printf '%s' "$result" | cut -d' ' -f3) ;
+    local result_h ; result_h=$(printf '%s' "$result" | cut -d' ' -f5) ;
+    local result_d ; result_d=$(printf '%s' "$result" | cut -d' ' -f7) ;
+    local expect_u ; expect_u="'127.0.0.1:9650/ext/keystore'" ;
+    assertEquals "$expect_u" "$result_u" ;
+    local expect_h ; expect_h="'content-type:application/json'" ;
+    assertEquals "$expect_h" "$result_h" ;
+    local expect_d ; expect_d="'{" ;
+    expect_d+='"jsonrpc":"2.0",' ;
+    expect_d+='"id":1,' ;
+    expect_d+='"method":"keystore.importUser",' ;
+    expect_d+='"params":{' ;
+    expect_d+='"username":"USERNAME",' ;
+    expect_d+='"password":"PASSWORD",' ;
+    expect_d+='"user":"USER"' ;
+    expect_d+="}}'" ;
+    assertEquals "$expect_d" "$result_d" ;
+    local expect="curl --url $expect_u --header $expect_h --data $expect_d" ;
+    assertEquals "$expect" "$result" ;
+}
+
+function test_keystore__import_user_1a {
+    check "$(RPC_ID=1 $(cmd) -u USERNAME -p PASSWORD -U USER)" ;
+}
+
+function test_keystore__import_user_1b {
+    check "$(RPC_ID=1 AVA_USERNAME=USERNAME $(cmd) -p PASSWORD -U USER)" ;
+}
+
+function test_keystore__import_user_1c {
+    check "$(RPC_ID=1 AVA_PASSWORD=PASSWORD $(cmd) -u USERNAME -U USER)" ;
+}
+
+function test_keystore__import_user_1d {
+    check "$(RPC_ID=1 AVA_USER=USER $(cmd) -u USERNAME -p PASSWORD)" ;
+}
+
+###############################################################################
+###############################################################################

cmd/keystore/list-users.test.sh
@@ -1,0 +1,34 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2091
+###############################################################################
+
+function cmd {
+    printf "./ava-cli.sh keystore list-users" ;
+}
+
+function check {
+    local result="$1" ;
+    local result_u ; result_u=$(printf '%s' "$result" | cut -d' ' -f3) ;
+    local result_h ; result_h=$(printf '%s' "$result" | cut -d' ' -f5) ;
+    local result_d ; result_d=$(printf '%s' "$result" | cut -d' ' -f7) ;
+    local expect_u ; expect_u="'127.0.0.1:9650/ext/keystore'" ;
+    assertEquals "$expect_u" "$result_u" ;
+    local expect_h ; expect_h="'content-type:application/json'" ;
+    assertEquals "$expect_h" "$result_h" ;
+    local expect_d ; expect_d="'{" ;
+    expect_d+='"jsonrpc":"2.0",' ;
+    expect_d+='"id":1,' ;
+    expect_d+='"method":"keystore.listUsers",' ;
+    expect_d+='"params":{' ;
+    expect_d+="}}'" ;
+    assertEquals "$expect_d" "$result_d" ;
+    local expect="curl --url $expect_u --header $expect_h --data $expect_d" ;
+    assertEquals "$expect" "$result" ;
+}
+
+function test_keystore__list_users {
+    check "$(RPC_ID=1 $(cmd))" ;
+}
+
+###############################################################################
+###############################################################################

blackhan-software/avalanche-cli

cmd/keystore/create-user.sh
@@ -1,0 +1,98 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC1090,SC2214
+###############################################################################
+CMD_SCRIPT=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)
+###############################################################################
+
+source "$CMD_SCRIPT/../../cli/color.sh" ;
+source "$CMD_SCRIPT/../../cli/command.sh" ;
+source "$CMD_SCRIPT/../../cli/rpc/data.sh" ;
+source "$CMD_SCRIPT/../../cli/rpc/post.sh" ;
+
+###############################################################################
+###############################################################################
+
+function cli_help {
+    local usage ;
+    usage="${BB}Usage:${NB} $(command_fqn "${0}")" ;
+    usage+=" [-u|--username=\${AVA_USERNAME}]" ;
+    usage+=" [-p|--password=\${AVA_PASSWORD}]" ;
+    usage+=" [-N|--node=\${AVA_NODE-127.0.0.1:9650}]" ;
+    usage+=" [-S|--silent-rpc|\${AVA_SILENT_RPC}]" ;
+    usage+=" [-V|--verbose-rpc|\${AVA_VERBOSE_RPC}]" ;
+    usage+=" [-Y|--yes-run-rpc|\${AVA_YES_RUN_RPC}]" ;
+    usage+=" [-h|--help]" ;
+    source "$CMD_SCRIPT/../../cli/help.sh" ; # shellcheck disable=2046
+    printf '%s\n\n%s\n' "$usage" "$(help_for $(command_fqn "${0}"))" ;
+}
+
+function cli_options {
+    local -a options ;
+    options+=( "-u" "--username=" ) ;
+    options+=( "-p" "--password=" ) ;
+    options+=( "-N" "--node=" ) ;
+    options+=( "-S" "--silent-rpc" ) ;
+    options+=( "-V" "--verbose-rpc" ) ;
+    options+=( "-Y" "--yes-run-rpc" ) ;
+    options+=( "-h" "--help" ) ;
+    printf '%s ' "${options[@]}" ;
+}
+
+function cli {
+    while getopts ":hSVYN:u:p:-:" OPT "$@"
+    do
+        if [ "$OPT" = "-" ] ; then
+            OPT="${OPTARG%%=*}" ;
+            OPTARG="${OPTARG#$OPT}" ;
+            OPTARG="${OPTARG#=}" ;
+        fi
+        case "${OPT}" in
+            list-options)
+                cli_options && exit 0 ;;
+            u|username)
+                AVA_USERNAME="${OPTARG}" ;;
+            p|password)
+                AVA_PASSWORD="${OPTARG}" ;;
+            N|node)
+                AVA_NODE="${OPTARG}" ;;
+            S|silent-rpc)
+                export AVA_SILENT_RPC=1 ;;
+            V|verbose-rpc)
+                export AVA_VERBOSE_RPC=1 ;;
+            Y|yes-run-rpc)
+                export AVA_YES_RUN_RPC=1 ;;
+            h|help)
+                cli_help && exit 0 ;;
+            :|*)
+                cli_help && exit 1 ;;
+        esac
+    done
+    if [ -z "$AVA_USERNAME" ] ; then
+        cli_help && exit 1 ;
+    fi
+    if [ -z "$AVA_PASSWORD" ] ; then
+        cli_help && exit 1 ;
+    fi
+    if [ -z "$AVA_NODE" ] ; then
+        AVA_NODE="127.0.0.1:9650" ;
+    fi
+    shift $((OPTIND-1)) ;
+}
+
+function rpc_method {
+    printf "keystore.createUser" ;
+}
+
+function rpc_params {
+    printf '{' ;
+    printf '"username":"%s",' "$AVA_USERNAME" ;
+    printf '"password":"%s"' "$AVA_PASSWORD" ;
+    printf '}' ;
+}
+
+###############################################################################
+
+cli "$@" && rpc_post "$AVA_NODE/ext/keystore" "$(rpc_data)" ;
+
+###############################################################################
+###############################################################################

cmd/keystore/create-user.test.sh
@@ -1,0 +1,43 @@
+#!/usr/bin/env bash
+###############################################################################
+
+function cmd {
+    printf "./avalanche-cli.sh keystore create-user" ;
+}
+
+function check {
+    local result="$1" ;
+    local result_u ; result_u=$(printf '%s' "$result" | cut -d' ' -f3) ;
+    local result_h ; result_h=$(printf '%s' "$result" | cut -d' ' -f5) ;
+    local result_d ; result_d=$(printf '%s' "$result" | cut -d' ' -f7) ;
+    local expect_u ; expect_u="'127.0.0.1:9650/ext/keystore'" ;
+    assertEquals "$expect_u" "$result_u" ;
+    local expect_h ; expect_h="'content-type:application/json'" ;
+    assertEquals "$expect_h" "$result_h" ;
+    local expect_d ; expect_d="'{" ;
+    expect_d+='"jsonrpc":"2.0",' ;
+    expect_d+='"id":1,' ;
+    expect_d+='"method":"keystore.createUser",' ;
+    expect_d+='"params":{' ;
+    expect_d+='"username":"USERNAME",' ;
+    expect_d+='"password":"PASSWORD"' ;
+    expect_d+="}}'" ;
+    assertEquals "$expect_d" "$result_d" ;
+    local expect="curl --url $expect_u --header $expect_h --data $expect_d" ;
+    assertEquals "$expect" "$result" ;
+}
+
+function test_keystore__create_user_1a {
+    check "$(RPC_ID=1 $(cmd) -u USERNAME -p PASSWORD)" ;
+}
+
+function test_keystore__create_user_1b {
+    check "$(RPC_ID=1 AVA_USERNAME=USERNAME $(cmd) -p PASSWORD)" ;
+}
+
+function test_keystore__create_user_1c {
+    check "$(RPC_ID=1 AVA_PASSWORD=PASSWORD $(cmd) -u USERNAME)" ;
+}
+
+###############################################################################
+###############################################################################

cmd/keystore/delete-user.test.sh
@@ -1,0 +1,43 @@
+#!/usr/bin/env bash
+###############################################################################
+
+function cmd {
+    printf "./avalanche-cli.sh keystore delete-user" ;
+}
+
+function check {
+    local result="$1" ;
+    local result_u ; result_u=$(printf '%s' "$result" | cut -d' ' -f3) ;
+    local result_h ; result_h=$(printf '%s' "$result" | cut -d' ' -f5) ;
+    local result_d ; result_d=$(printf '%s' "$result" | cut -d' ' -f7) ;
+    local expect_u ; expect_u="'127.0.0.1:9650/ext/keystore'" ;
+    assertEquals "$expect_u" "$result_u" ;
+    local expect_h ; expect_h="'content-type:application/json'" ;
+    assertEquals "$expect_h" "$result_h" ;
+    local expect_d ; expect_d="'{" ;
+    expect_d+='"jsonrpc":"2.0",' ;
+    expect_d+='"id":1,' ;
+    expect_d+='"method":"keystore.deleteUser",' ;
+    expect_d+='"params":{' ;
+    expect_d+='"username":"USERNAME",' ;
+    expect_d+='"password":"PASSWORD"' ;
+    expect_d+="}}'" ;
+    assertEquals "$expect_d" "$result_d" ;
+    local expect="curl --url $expect_u --header $expect_h --data $expect_d" ;
+    assertEquals "$expect" "$result" ;
+}
+
+function test_keystore__delete_user_1a {
+    check "$(RPC_ID=1 $(cmd) -u USERNAME -p PASSWORD)" ;
+}
+
+function test_keystore__delete_user_1b {
+    check "$(RPC_ID=1 AVA_USERNAME=USERNAME $(cmd) -p PASSWORD)" ;
+}
+
+function test_keystore__delete_user_1c {
+    check "$(RPC_ID=1 AVA_PASSWORD=PASSWORD $(cmd) -u USERNAME)" ;
+}
+
+###############################################################################
+###############################################################################

cmd/keystore/export-user.sh
@@ -1,0 +1,98 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC1090,SC2214
+###############################################################################
+CMD_SCRIPT=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)
+###############################################################################
+
+source "$CMD_SCRIPT/../../cli/color.sh" ;
+source "$CMD_SCRIPT/../../cli/command.sh" ;
+source "$CMD_SCRIPT/../../cli/rpc/data.sh" ;
+source "$CMD_SCRIPT/../../cli/rpc/post.sh" ;
+
+###############################################################################
+###############################################################################
+
+function cli_help {
+    local usage ;
+    usage="${BB}Usage:${NB} $(command_fqn "${0}")" ;
+    usage+=" [-u|--username=\${AVA_USERNAME}]" ;
+    usage+=" [-p|--password=\${AVA_PASSWORD}]" ;
+    usage+=" [-N|--node=\${AVA_NODE-127.0.0.1:9650}]" ;
+    usage+=" [-S|--silent-rpc|\${AVA_SILENT_RPC}]" ;
+    usage+=" [-V|--verbose-rpc|\${AVA_VERBOSE_RPC}]" ;
+    usage+=" [-Y|--yes-run-rpc|\${AVA_YES_RUN_RPC}]" ;
+    usage+=" [-h|--help]" ;
+    source "$CMD_SCRIPT/../../cli/help.sh" ; # shellcheck disable=2046
+    printf '%s\n\n%s\n' "$usage" "$(help_for $(command_fqn "${0}"))" ;
+}
+
+function cli_options {
+    local -a options ;
+    options+=( "-u" "--username=" ) ;
+    options+=( "-p" "--password=" ) ;
+    options+=( "-N" "--node=" ) ;
+    options+=( "-S" "--silent-rpc" ) ;
+    options+=( "-V" "--verbose-rpc" ) ;
+    options+=( "-Y" "--yes-run-rpc" ) ;
+    options+=( "-h" "--help" ) ;
+    printf '%s ' "${options[@]}" ;
+}
+
+function cli {
+    while getopts ":hSVYN:u:p:-:" OPT "$@"
+    do
+        if [ "$OPT" = "-" ] ; then
+            OPT="${OPTARG%%=*}" ;
+            OPTARG="${OPTARG#$OPT}" ;
+            OPTARG="${OPTARG#=}" ;
+        fi
+        case "${OPT}" in
+            list-options)
+                cli_options && exit 0 ;;
+            u|username)
+                AVA_USERNAME="${OPTARG}" ;;
+            p|password)
+                AVA_PASSWORD="${OPTARG}" ;;
+            N|node)
+                AVA_NODE="${OPTARG}" ;;
+            S|silent-rpc)
+                export AVA_SILENT_RPC=1 ;;
+            V|verbose-rpc)
+                export AVA_VERBOSE_RPC=1 ;;
+            Y|yes-run-rpc)
+                export AVA_YES_RUN_RPC=1 ;;
+            h|help)
+                cli_help && exit 0 ;;
+            :|*)
+                cli_help && exit 1 ;;
+        esac
+    done
+    if [ -z "$AVA_USERNAME" ] ; then
+        cli_help && exit 1 ;
+    fi
+    if [ -z "$AVA_PASSWORD" ] ; then
+        cli_help && exit 1 ;
+    fi
+    if [ -z "$AVA_NODE" ] ; then
+        AVA_NODE="127.0.0.1:9650" ;
+    fi
+    shift $((OPTIND-1)) ;
+}
+
+function rpc_method {
+    printf "keystore.exportUser" ;
+}
+
+function rpc_params {
+    printf '{' ;
+    printf '"username":"%s",' "$AVA_USERNAME" ;
+    printf '"password":"%s"' "$AVA_PASSWORD" ;
+    printf '}' ;
+}
+
+###############################################################################
+
+cli "$@" && rpc_post "$AVA_NODE/ext/keystore" "$(rpc_data)" ;
+
+###############################################################################
+###############################################################################

cmd/keystore/import-user.test.sh
@@ -1,0 +1,48 @@
+#!/usr/bin/env bash
+###############################################################################
+
+function cmd {
+    printf "./avalanche-cli.sh keystore import-user" ;
+}
+
+function check {
+    local result="$1" ;
+    local result_u ; result_u=$(printf '%s' "$result" | cut -d' ' -f3) ;
+    local result_h ; result_h=$(printf '%s' "$result" | cut -d' ' -f5) ;
+    local result_d ; result_d=$(printf '%s' "$result" | cut -d' ' -f7) ;
+    local expect_u ; expect_u="'127.0.0.1:9650/ext/keystore'" ;
+    assertEquals "$expect_u" "$result_u" ;
+    local expect_h ; expect_h="'content-type:application/json'" ;
+    assertEquals "$expect_h" "$result_h" ;
+    local expect_d ; expect_d="'{" ;
+    expect_d+='"jsonrpc":"2.0",' ;
+    expect_d+='"id":1,' ;
+    expect_d+='"method":"keystore.importUser",' ;
+    expect_d+='"params":{' ;
+    expect_d+='"username":"USERNAME",' ;
+    expect_d+='"password":"PASSWORD",' ;
+    expect_d+='"user":"USER"' ;
+    expect_d+="}}'" ;
+    assertEquals "$expect_d" "$result_d" ;
+    local expect="curl --url $expect_u --header $expect_h --data $expect_d" ;
+    assertEquals "$expect" "$result" ;
+}
+
+function test_keystore__import_user_1a {
+    check "$(RPC_ID=1 $(cmd) -u USERNAME -p PASSWORD -U USER)" ;
+}
+
+function test_keystore__import_user_1b {
+    check "$(RPC_ID=1 AVA_USERNAME=USERNAME $(cmd) -p PASSWORD -U USER)" ;
+}
+
+function test_keystore__import_user_1c {
+    check "$(RPC_ID=1 AVA_PASSWORD=PASSWORD $(cmd) -u USERNAME -U USER)" ;
+}
+
+function test_keystore__import_user_1d {
+    check "$(RPC_ID=1 AVA_USER=USER $(cmd) -u USERNAME -p PASSWORD)" ;
+}
+
+###############################################################################
+###############################################################################

cmd/keystore/list-users.test.sh
@@ -1,0 +1,34 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2091
+###############################################################################
+
+function cmd {
+    printf "./avalanche-cli.sh keystore list-users" ;
+}
+
+function check {
+    local result="$1" ;
+    local result_u ; result_u=$(printf '%s' "$result" | cut -d' ' -f3) ;
+    local result_h ; result_h=$(printf '%s' "$result" | cut -d' ' -f5) ;
+    local result_d ; result_d=$(printf '%s' "$result" | cut -d' ' -f7) ;
+    local expect_u ; expect_u="'127.0.0.1:9650/ext/keystore'" ;
+    assertEquals "$expect_u" "$result_u" ;
+    local expect_h ; expect_h="'content-type:application/json'" ;
+    assertEquals "$expect_h" "$result_h" ;
+    local expect_d ; expect_d="'{" ;
+    expect_d+='"jsonrpc":"2.0",' ;
+    expect_d+='"id":1,' ;
+    expect_d+='"method":"keystore.listUsers",' ;
+    expect_d+='"params":{' ;
+    expect_d+="}}'" ;
+    assertEquals "$expect_d" "$result_d" ;
+    local expect="curl --url $expect_u --header $expect_h --data $expect_d" ;
+    assertEquals "$expect" "$result" ;
+}
+
+function test_keystore__list_users {
+    check "$(RPC_ID=1 $(cmd))" ;
+}
+
+###############################################################################
+###############################################################################