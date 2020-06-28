ad0b0d96a04807ed7bd71d94ae3ac202081b4045
Hasan Karahan <hasan.karahan@blackhan.com>
2020-06-28 15:24:05 +0300

    CLI: cli/*.sh

--- cli/si-suffix.sh
+++ cli/si-suffix.sh
@@ -2,15 +2,13 @@
 ###############################################################################
 
 function si {
-    local number_rx="([[:digit:]]+)[YZEPTGMK]?" ;
+    local number_rx="([[:digit:]]+)[EPTGMK]?" ;
     local number; number="$(\
-        [[ "$1" =~ $number_rx ]] && printf '%s' "${BASH_REMATCH[1]}")"
-    local metric_rx="[[:digit:]]+([YZEPTGMK]?)" ;
+        [[ "$1" =~ $number_rx ]] && printf '%s' "${BASH_REMATCH[1]}")" ;
+    local metric_rx="[[:digit:]]+([EPTGMK]?)" ;
     local metric; metric="$(\
-        [[ "$1" =~ $metric_rx ]] && printf '%s' "${BASH_REMATCH[1]}")"
-    case "$metric" in
-        Y) printf '%s000000000000000000000000' "$number" ;;
-        Z) printf '%s000000000000000000000' "$number" ;;
+        [[ "$1" =~ $metric_rx ]] && printf '%s' "${BASH_REMATCH[1]}")" ;
+    if [ -n "$number" ] ; then case "$metric" in
         E) printf '%s000000000000000000' "$number" ;;
         P) printf '%s000000000000000' "$number" ;;
         T) printf '%s000000000000' "$number" ;;
@@ -19,6 +17,9 @@ function si {
         K) printf '%s000' "$number" ;;
         *) printf '%s' "$number" ;;
     esac
+    else
+        printf '0' ;
+    fi
 }
 
 ###############################################################################
